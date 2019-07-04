54db68dbfb06357dc1ace09a0b4069920d01649a
Lauri <lauri@xip.fi>
2019-07-04 15:42:36 +0300

    added a test for scalajs analyzer

---
+++ src/test/resources/test_data/script.scala
@@ -0,0 +1,45 @@
+
+
+object ScalaJSAnalyzer {
+
+  val AnalyzersDataInternal = g.Java.applyDynamic("type")("com.getjenny.analyzer.expressions.AnalyzersDataInternal")
+  val Result = g.Java.applyDynamic("type")("com.getjenny.analyzer.expressions.Result")
+
+  def main(args: Array[String]): Unit = {
+
+    val pattern = """my name is (\w+)""".r
+
+    // get sentence and analyzersDataInternal from global scope
+    val sentence = g.sentence.asInstanceOf[String]
+
+    // apply regex
+    val name = pattern.findFirstMatchIn(sentence) match {
+      case Some(m) => m.group(1)
+      case None => ""
+    }
+
+    val analyzersDataInternal = g.analyzersDataInternal
+
+    val data = analyzersDataInternal.applyDynamic("data")()
+    val context = analyzersDataInternal.applyDynamic("context")()
+    val traversedStates = analyzersDataInternal.applyDynamic("traversedStates")()
+
+    // if name found, store it!
+    val (extractedVariables, score) = if(name == ""){
+      (analyzersDataInternal.applyDynamic("extractedVariables")(), 0)
+    } else {
+      (analyzersDataInternal.applyDynamic("extractedVariables")()
+        .applyDynamic("updated")("name", name), 1)
+    }
+
+    // create new AnalyzersDataInternal object
+    val dataInternal = AnalyzersDataInternal.applyDynamic("apply")(context, traversedStates, extractedVariables, data)
+
+    // create new Result object
+    val result = Result.applyDynamic("apply")(score, dataInternal)
+
+    g.result = result
+
+  }
+}
+
--- src/test/scala/com/getjenny/starchat/resources/AnalyzersPlaygroundResourceTest.scala
+++ src/test/scala/com/getjenny/starchat/resources/AnalyzersPlaygroundResourceTest.scala
@@ -12,6 +12,7 @@ import com.getjenny.starchat.utils.Index
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.concurrent.duration._
+import scala.io.Source
 
 class AnalyzersPlaygroundResourceTest extends WordSpec with Matchers with ScalatestRouteTest with JsonSupport {
   implicit def default(implicit system: ActorSystem) = RouteTestTimeout(10.seconds.dilated(system))
@@ -367,6 +368,38 @@ class AnalyzersPlaygroundResourceTest extends WordSpec with Matchers with Scalat
         response.data.getOrElse(fail).traversedStates should be (Vector("one", "two"))
         response.data.getOrElse(fail).extractedVariables("lng") should be ("en")
         response.value should be (0)
+      }
+    }
+  }
+
+  it should {
+    val script = Source.fromResource("test_data/script.scala").getLines().mkString("\n")
+
+    "return an HTTP code 200 when evaluating a ScalaJS analyzer" in {
+      val analyzerData = AnalyzersData(traversedStates=Vector("one", "two"),
+        extractedVariables =
+          Map[String, String](
+            "month.0" -> "11",
+            "day.0" -> "31",
+            "year.0" -> "1900"))
+
+      val evaluateRequest: AnalyzerEvaluateRequest =
+        AnalyzerEvaluateRequest(
+          query = "my name is Jenny",
+          analyzer = "type/SCALAJS\n"+script,
+          data = Option{
+            analyzerData
+          }
+        )
+
+      Post(s"/index_getjenny_english_0/analyzer/playground", evaluateRequest) ~>
+        addCredentials(testUserCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        val response = responseAs[AnalyzerEvaluateResponse]
+        response.build should be (true)
+        response.buildMessage should be ("success")
+        response.value should be (1)
+        response.data.getOrElse(fail("no AnalyzerData")) should be (analyzerData.copy(extractedVariables = analyzerData.extractedVariables + ("name" -> "Jenny")))
       }
     }
   }
