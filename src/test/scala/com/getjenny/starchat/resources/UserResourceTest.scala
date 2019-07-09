f2e752085deffb85e224541ff422efe84c532667
Lauri <lauri@xip.fi>
2019-07-09 10:00:45 +0300

    Added fail test cases

--- src/test/scala/com/getjenny/starchat/resources/UserResourceTest.scala
+++ src/test/scala/com/getjenny/starchat/resources/UserResourceTest.scala
@@ -3,6 +3,7 @@ package com.getjenny.starchat.resources
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.headers.BasicHttpCredentials
+import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import akka.testkit._
 import com.getjenny.starchat.entities._
@@ -18,6 +19,7 @@ class UserResourceTest extends WordSpec with Matchers with ScalatestRouteTest wi
   val routes = service.routes
 
   val testAdminCredentials = BasicHttpCredentials("admin", "adminp4ssw0rd")
+  val userCredentials = BasicHttpCredentials("AzureDiamond", "hunter3")
 
   "StarChat" should {
     "return an HTTP code 201 when creating a new system index" in {
@@ -102,6 +104,55 @@ class UserResourceTest extends WordSpec with Matchers with ScalatestRouteTest wi
           val response = responseAs[UserUpdate]
           response shouldEqual updatedUser
       })
+    }
+  }
+
+  it should {
+    "return an HTTP code 403 when not having admin permissions" in {
+      val userUpdate = UserUpdate(
+        id = "AzureDiamond",
+        password = Some("hunter3"),
+        salt = Some("smoked salt"),
+        permissions = Some(Map("index_getjenny_english_0" -> Set(
+          Permissions.read, Permissions.write, Permissions.admin, Permissions.stream)))
+      )
+      Post("/user/generate", userUpdate) ~> addCredentials(userCredentials) ~> Route.seal(routes) ~> check {
+        status shouldEqual StatusCodes.Forbidden
+      }
+      Put("/user", userUpdate) ~> addCredentials(userCredentials) ~> Route.seal(routes) ~> check {
+        status shouldEqual StatusCodes.Forbidden
+      }
+      Post("/user/get", UserId(userUpdate.id)) ~> addCredentials(userCredentials) ~> Route.seal(routes) ~> check {
+        status shouldEqual StatusCodes.Forbidden
+      }
+      Post("/user/delete", UserId(userUpdate.id)) ~> addCredentials(userCredentials) ~> Route.seal(routes) ~> check {
+        status shouldEqual StatusCodes.Forbidden
+      }
+    }
+  }
+
+  it should {
+    "return an HTTP code 401 when unauthorized" in {
+      val fakeCredentials = BasicHttpCredentials("admin", "admin")
+      val userUpdate = UserUpdate(
+        id = "admin",
+        password = Some("admin"),
+        salt = Some("smelly salt"),
+        permissions = Some(Map("index_getjenny_english_0" -> Set(
+          Permissions.read, Permissions.write, Permissions.admin, Permissions.stream)))
+      )
+      Post("/user/generate", userUpdate) ~> addCredentials(fakeCredentials) ~> Route.seal(routes) ~> check {
+        status shouldEqual StatusCodes.Unauthorized
+      }
+      Put("/user", userUpdate) ~> addCredentials(fakeCredentials) ~> Route.seal(routes) ~> check {
+        status shouldEqual StatusCodes.Unauthorized
+      }
+      Post("/user/get", UserId(userUpdate.id)) ~> addCredentials(fakeCredentials) ~> Route.seal(routes) ~> check {
+        status shouldEqual StatusCodes.Unauthorized
+      }
+      Post("/user/delete", UserId("admin")) ~> addCredentials(fakeCredentials) ~> Route.seal(routes) ~> check {
+        status shouldEqual StatusCodes.Unauthorized
+      }
     }
   }
 
