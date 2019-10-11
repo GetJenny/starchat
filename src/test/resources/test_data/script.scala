62e9d146bc78efc5579fe27b362cf370a5b585f6
Lauri <lauri@xip.fi>
2019-10-11 16:40:52 +0300

    updated test script

--- src/test/resources/test_data/script.scala
+++ src/test/resources/test_data/script.scala
@@ -1,45 +1,37 @@
 import scala.scalajs.js.Dynamic.{ global => g }
 
-object ScalaJSAnalyzer {
+val AnalyzersDataInternal = g.Java.applyDynamic("type")("com.getjenny.analyzer.expressions.AnalyzersDataInternal")
+val Result = g.Java.applyDynamic("type")("com.getjenny.analyzer.expressions.Result")
 
-  val AnalyzersDataInternal = g.Java.applyDynamic("type")("com.getjenny.analyzer.expressions.AnalyzersDataInternal")
-  val Result = g.Java.applyDynamic("type")("com.getjenny.analyzer.expressions.Result")
+val pattern = """my name is (\w+)""".r
 
-  def main(args: Array[String]): Unit = {
+// get sentence and analyzersDataInternal from global scope
+val sentence = g.sentence.asInstanceOf[String]
 
-    val pattern = """my name is (\w+)""".r
-
-    // get sentence and analyzersDataInternal from global scope
-    val sentence = g.sentence.asInstanceOf[String]
-
-    // apply regex
-    val name = pattern.findFirstMatchIn(sentence) match {
-      case Some(m) => m.group(1)
-      case None => ""
-    }
-
-    val analyzersDataInternal = g.analyzersDataInternal
-
-    val data = analyzersDataInternal.applyDynamic("data")()
-    val context = analyzersDataInternal.applyDynamic("context")()
-    val traversedStates = analyzersDataInternal.applyDynamic("traversedStates")()
-
-    // if name found, store it!
-    val (extractedVariables, score) = if(name == ""){
-      (analyzersDataInternal.applyDynamic("extractedVariables")(), 0)
-    } else {
-      (analyzersDataInternal.applyDynamic("extractedVariables")()
-        .applyDynamic("updated")("name", name), 1)
-    }
-
-    // create new AnalyzersDataInternal object
-    val dataInternal = AnalyzersDataInternal.applyDynamic("apply")(context, traversedStates, extractedVariables, data)
-
-    // create new Result object
-    val result = Result.applyDynamic("apply")(score, dataInternal)
-
-    g.result = result
-
-  }
+// apply regex
+val name = pattern.findFirstMatchIn(sentence) match {
+  case Some(m) => m.group(1)
+  case None => ""
 }
 
+val analyzersDataInternal = g.analyzersDataInternal
+
+val data = analyzersDataInternal.applyDynamic("data")()
+val context = analyzersDataInternal.applyDynamic("context")()
+val traversedStates = analyzersDataInternal.applyDynamic("traversedStates")()
+
+// if name found, store it!
+val (extractedVariables, score) = if(name == ""){
+  (analyzersDataInternal.applyDynamic("extractedVariables")(), 0)
+} else {
+  (analyzersDataInternal.applyDynamic("extractedVariables")()
+    .applyDynamic("updated")("name", name), 1)
+}
+
+// create new AnalyzersDataInternal object
+val dataInternal = AnalyzersDataInternal.applyDynamic("apply")(context, traversedStates, extractedVariables, data)
+
+// create new Result object
+val result = Result.applyDynamic("apply")(score, dataInternal)
+
+g.result = result
